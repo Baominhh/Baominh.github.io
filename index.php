4f479f9cdc4958186b6a970ad0cbd8a7c6d7aecc
Baominhh <139488285+Baominhh@users.noreply.github.com>
2023-07-13 23:46:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,94 @@
+<?php include 'layouts/header.php'; ?>
+<?php include 'layouts/nav.php'; ?>
+<div class="ta-content">
+    <div class="shop-profile">
+        <?php include 'layouts/avatar.php'; ?>
+        <div class="ta-container pt-3">
+            <div class="shop-navigation">
+                <div class="row gx-sm-3 gx-2">
+                    <div class="col-4">
+                        <div class="profile-navigation home active"><a href="/"
+                                class="nuxt-link-exact-active nuxt-link-active" aria-current="page"><i
+                                    class="fad fa-home"></i> <span class="text">Cá nhân</span></a></div>
+                    </div>
+                    <div class="col-4">
+                        <div class="profile-navigation review"><a href="/banking" class=""><i class="fad fa-star"></i>
+                                <span class="text">Thanh toán</span></a></div>
+                    </div>
+                    <div class="col-4">
+                        <div class="profile-navigation community"><a href="/demo" class=""><i
+                                    class="fad fa-flag"></i> <span class="text">Cộng đồng</span></a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="ta-content">
+            <div class="ta-container py-3 pb-sm-4 pt-sm-3">
+                <div>
+                    <div class="row flex-row-reverse">
+                        <div class="col-lg-6">
+                            <div class="card">
+                                <div data-bs-toggle="tooltip" data-bs-title="Default tooltip" class="card-body">
+                                    <h5 class="card-title text-danger"><i class="fad fa-star"></i> Đình đám một thời:
+                                    </h5>
+                                    <p class="mb-0 fs-14">
+                                        Từ những năm 2022, Đình Bảo nổi lên như cồn về độ chịu chơi khi thanh toán bịch
+                                        bim bim đưa 5k không cần thối lại, trên mảnh đất Hà Nội không ai là biết đến
+                                        Nguyễn Đình Bảo vì Bảo sống ở Cam Ranh. Từng một mình hỗn chiến với 8 người máu
+                                        mặt lúc bấy giờ tại cổng trời (Đảo quân sự). Một mình cân 50 thằng chỉ với một
+                                        chiếc cân
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="card">
+                                <div data-bs-toggle="tooltip" data-bs-title="Default tooltip" class="card-body">
+                                    <h5 class="card-title text-danger"><i class="fad fa-user"></i> Thông tin về Vani:
+                                    </h5>
+                                    <p class="mb-0 fs-14">
+                                        Đình Bảo là một chàng trai trẻ sinh năm 2006 đến từ Cam Ranh, Việt Nam. Anh ta
+                                        đã phát hiện ra niềm đam mê và tài năng trong lĩnh vực thiết kế website. Với sự
+                                        sáng tạo và khả năng thẩm mỹ của mình, Đình Bảo đã sẵn sàng đối mặt với những
+                                        thách thức trong việc tạo ra giao diện trực quan và hấp dẫn cho các trang web.
+                                        (tất cả là xao lon)
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="card">
+                                <div data-bs-toggle="tooltip" data-bs-title="Default tooltip" class="card-body">
+                                    <h5 class="card-title text-danger"><i class="fad fa-mobile"></i> Liên hệ:</h5>
+                                    <p class="mb-0 fs-14">
+                                        Facebook: <a href="https://facebook.com/vanixjnk">@vanixjnk</a><br>
+                                        Facebook: <a href="https://facebook.com/vani.realest">@vani.realest</a><br>
+                                        Instagram: <a href="https://instagram.com/vanixjnk">@vanixjnh</a><br>
+                                        Instagram: <a href="https://instagram.com/vani.realest">@vani.realest</a><br>
+                                        Tiktok: <a href="https://tiktok.com/@vanixjnk">@vanixjnk</a>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="card">
+                                <div data-bs-toggle="tooltip" data-bs-title="Default tooltip" class="card-body">
+                                    <h5 class="card-title text-danger"><i class="fad fa-sensor-alert"></i> Kỹ năng và
+                                        kinh nghiệm:</h5>
+                                    <p class="mb-0 fs-14">
+                                        Thiết kế giao diện web <br>
+                                        Lập trình HTML, CSS, JavaScript <br>
+                                        Tối ưu hóa trang web cho công cụ tìm kiếm (SEO) <br>
+                                        Tương tác với người dùng và hiểu nhu cầu của họ <br>
+                                        (Những kỹ năng trên mõm đấy V:)
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include 'layouts/footer.php'; ?>
