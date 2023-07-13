0050c9fe50c1c679c63ad55cb78aba4c7a00b918
Baominhh <139488285+Baominhh@users.noreply.github.com>
2023-07-13 11:24:08 -0700

    Add files via upload

---
+++ codeprofile/layouts/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html mode="">
+
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Vani.tv | Thông tin cá nhân</title>
+    <meta name="description" content="Vani.tv | Thông tin cá nhân">
+    <meta name="keywords" content="Vani.tv | Thông tin cá nhân">
+    <meta name="author" content="Vani">
+    <meta name="theme-color" content="#fff">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400&display=swap" rel="stylesheet">
+    <link rel='stylesheet' type='text/css' media='screen' href='assets/css/style.css'>
+    <link rel='stylesheet' type='text/css' media='screen' href='assets/css/app.css?'>
+    <link rel='stylesheet' type='text/css' media='screen' href='assets/css/animation.css'>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
